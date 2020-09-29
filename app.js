2606e2d26e11ae0f7d3e69ee71a48e5a55bf089f
bofletcher <hfletche.hf@gmail.com>
2020-09-29 15:23:40 -0400

    Showing dan stuff

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-	  res.send('Hello H3XPR00F, you are a W1Z4RD')
+	  res.send('Hello BO! and DAN!')
 })
 
 app.listen(port, () => {
